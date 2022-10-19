59ccefe8a986e693178bd3cef493878ccc1dd594
Clonkk <rf.clonk@linuxmail.org>
2022-10-19 12:02:00 +0200

    fix array creation ?

--- arrayfire/aftensors.nim
+++ arrayfire/aftensors.nim
@@ -6,7 +6,7 @@ type
     array*: AFArray
     dtype*: Dtype
 
-proc initAFTensor*[T]() : AFTensor[T] =
+proc initAFTensor*[T](): AFTensor[T] =
   result.dtype = getDtype[T]()
   result.array = afa()
 
@@ -19,29 +19,30 @@ proc fromAFArray*[T](result: var AFTensor[T], m: sink AFArray) =
   else:
     result.array = m.`as`(result.dtype)
 
-proc initAFTensor*[T](m: sink AFArray) : AFTensor[T] =
+proc initAFTensor*[T](m: sink AFArray): AFTensor[T] =
   fromAFArray(result, m)
 
-proc initAFTensor*[T](dims: Dim4, datas: openArray[T]) : AFTensor[T] =
+proc initAFTensor*[T](dims: Dim4, datas: openArray[T]): AFTensor[T] =
   result.dtype = getDtype[T]()
   result.array = sugars.afa(dims, datas, result.dtype)
 
-proc initAFTensor*[T](dim0: DimT, datas: openArray[T]) : AFTensor[T] =
+proc initAFTensor*[T](dim0: DimT, datas: openArray[T]): AFTensor[T] =
   result.dtype = getDtype[T]()
   result.array = sugars.afa(dim0, datas, result.dtype)
 
-proc initAFTensor*[T](dim0: DimT, dim1: DimT, datas: openArray[T]) : AFTensor[T] =
+proc initAFTensor*[T](dim0: DimT, dim1: DimT, datas: openArray[T]): AFTensor[T] =
   result.dtype = getDtype[T]()
   result.array = sugars.afa(dim0, dim1, datas, result.dtype)
 
-proc initAFTensor*[T](dim0: DimT, dim1: DimT, dim2: DimT, datas: openArray[T]) : AFTensor[T] =
+proc initAFTensor*[T](dim0: DimT, dim1: DimT, dim2: DimT, datas: openArray[T]): AFTensor[T] =
   result.dtype = getDtype[T]()
   result.array = sugars.afa(dim0, dim1, dim2, datas, result.dtype)
 
-proc initAFTensor*[T](dim0: DimT, dim1: DimT, dim2: DimT, dim3: DimT, datas: openArray[T]) : AFTensor[T] =
+proc initAFTensor*[T](dim0: DimT, dim1: DimT, dim2: DimT, dim3: DimT, datas: openArray[T]): AFTensor[T] =
   result.dtype = getDtype[T]()
   result.array = sugars.afa(dim0, dim1, dim2, dim3, datas, result.dtype)
 
+# TODO Figure out Slice
 # proc initAFTensor*[T](dims: Dim4, slice: Slice[T]) : AFTensor[T] =
 #   result.dtype = getDtype[T]()
 #   result.array = sugars.afa(dims, slice, result.dtype)
@@ -61,9 +62,9 @@ proc initAFTensor*[T](dim0: DimT, dim1: DimT, dim2: DimT, dim3: DimT, datas: ope
 # proc initAFTensor*[T](dim0: DimT, dim1: DimT, dim2: DimT, dim3: DimT, datas: Slice[T]) : AFTensor[T] =
 #   result.dtype = getDtype[T]()
 #   result.array = sugars.afa(dim0, dim1, dim2, dim3, datas, result.dtype)
-#
 
-proc `$`*[T](x: AFTensor[T]) : string =
+
+proc `$`*[T](x: AFTensor[T]): string =
   result.add "AFTensor("
   result.add "dtype:" & $(x.dtype) & ",\n"
   result.add "array:" & $(x.array)
